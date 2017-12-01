0dbc81d7784f43a9d84015ecd93742e6f8f749f3
Leighzer <bwc_nothgiel@live.com>
2017-12-01 14:13:01 -0800

    Update PlayingBingoCard.java

--- app/src/main/java/com/example/kelseybedard/roadtripbingo/PlayingBingoCard.java
+++ app/src/main/java/com/example/kelseybedard/roadtripbingo/PlayingBingoCard.java
@@ -24,7 +24,55 @@ public class PlayingBingoCard {
 
     //Will check if a bingo has taken place, will happen each time a icon is selected/unselected
     public boolean checkBingo(){
-        return false;
+        if(gameType.equals("Four Corners")){
+            return board.get(0).isSelected && board.get(4).isSelected && board.get(20).isSelected && board.get(24).isSelected;
+        }
+        else if(gameType.equals("Blackout")){
+            boolean allSelected = true;
+            for(int i = 0; i < 25; i++){
+                if(!board.get(i).isSelected){
+                    allSelected = false;
+                    break;
+                }
+            }
+            return allSelected;
+        }
+        else if(gameType.equals("Line")){
+            int rowCounts[] = {0,0,0,0,0};
+            int columnCounts[] = {0,0,0,0,0};
+            int diagonalCounts[] = {0,0};
+
+            for(int i = 0; i < 5; i++){
+                for(int j = 0; j < 5; j++){
+                    if(board.get((j*5) + i).isSelected){
+                        rowCounts[i] += 1;
+                        columnCounts[j] += 1;
+                        if(i == j){
+                            diagonalCounts[0] += 1;
+                        }
+                        if(i + j == 4){
+                            diagonalCounts[1] += 1;
+                        }
+                    }
+                }
+            }
+
+            for(int i = 0; i < 5; i++){
+                if(rowCounts[i] == 5 || columnCounts[i] == 5){
+                    return true;
+                }
+            }
+
+            if(diagonalCounts[0] == 5 || diagonalCounts[1] == 5){
+                return true;
+            }
+
+            return false;
+        }
+        else{
+            System.out.println("Invalid Game Type For Bingo Check");
+            return false;
+        }
     }
 
     //Here we will fill the board with random tiles the middle on being a free space, will return the list of strings for the icon image
